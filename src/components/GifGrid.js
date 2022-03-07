0d8392deafc6f033dbdde58a1c6d5a1c2d9b850b
Manuel Alejandro Cardona Morales <manuel.cardonam@autonoma.edu.co>
2022-03-06 21:26:50 -0500

    finish project

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,22 @@
 import React from 'react'
 import { useFetchGifs } from './hooks/useFetchGifs'
-//import { GifGridItem } from './GifGridItem';
-//import { getGifs } from './helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
+
 
 export const GifGrid = ({ category }) => {
 
-
-    // const [images, setImages] = useState([]);
-    const { loading } = useFetchGifs();
+    const { data:images, loading } = useFetchGifs( category );
 
     console.log(loading);
-    // /**
-    //  * Este useEffet permite que esa funcion que esta dentro no se ejecute si no cuando se renderiza el componente la primer vex
-    //  */
-    // useEffect( () => {
-    //     getGifs( category )
-    //         .then( setImages )
-    // }, [ category ]);
-
     
 
     return (
         <>
-            <h2> { category } </h2>
+            <h2 className='animate__animated animate__fadeIn'> { category } </h2>
 
-            { loading ? 'Cargando...' : 'Fin de carga' }
-            {/* <div className='card-grid'>
+            { loading && <p className='animate__animated animate__flash'>Loading...</p> }
+
+            <div className='card-grid'>
                 {
                     images.map( img => (
                         <GifGridItem 
@@ -35,7 +26,7 @@ export const GifGrid = ({ category }) => {
                         //<li key={ img.id }> { img.title } </li>
                     ))
                 }
-            </div> */}
+            </div>
         </>
     )
 }
